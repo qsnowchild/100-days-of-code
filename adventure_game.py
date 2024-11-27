4e60773e994c2e778dfade6a4dbf7ef95172ba08
renz ilagan <ilaganrenz01@gmail.com>
2024-11-27 13:49:23 +0800

    completed the adventure game, will polish still

--- adventure_game.py
+++ adventure_game.py
@@ -18,54 +18,72 @@ def ice():
 
 #create an enemy
 def enemy1():
-    print("Dark Frame")
+    return 1200
+
+#character
+def character():
+    return char_points
 
 
-gold_bank = 0
-inventory = []
+char_points = 0
 
 def movement():
-    global gold_bank
-    global inventory
-    # pick_path = input("Left or Right?: L/R: ").lower()
+    global char_points
     while True:
         pick_path = input("Left or Right?: L/R: ").lower()
         if pick_path == "l":
-            print("You have obtained 500 gold!")
-            gold_bank += 500
-            print(gold_bank)
+            print("You have obtained 500 points!")
+            char_points += 600
             break
         elif pick_path == "r":
-            print("You have obtained a Shiny Sword!")
-            inventory.append("Shiny Sword")
-            print(inventory)
+            print("You have obtained 300 points!")
+            char_points += 300
             break
         else:
             print("Please input a valid choice L/R")
 
-
 def action():
-    fight_nego = input("Do you wanna fight the monster? Y/N: ").lower()
+    global char_points
+    fight_nego = input(f"Do you wanna fight the monster? Y/N: Remember, the enemy has {enemy1()} points: ").lower()
     if fight_nego == "y":
-        enemy1()
-        print("Swings a sword")
+        print("Omega slash!!!!!!!!!!")
+        calc()
+    if fight_nego == "n":
+        print("Runs away, ill get back to you soon")
+        explore = input("Do you wanna explore other caves first?: ").lower()
+        if explore == "y":
+            firstmove()
+        else:
+            quit()
+
+
 
 playgame = input("Do you wanna play? Y/N: ").lower()
 if playgame != "y":
     quit()
 
+def calc():
+    char_points = character()
+    enemy_points = enemy1()
+
+    if char_points > enemy_points:
+        print("You won")
+    else:
+        print("You lost")
+
 print("Welcome Go Forth and Find the Treasure!!")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-print(f"Your current finds are {gold_bank} and {inventory}")
+def firstmove():
+    chooseapath = int(input("Each cave leads to different places, pick one! 1 / 2 / 3: "))
 
-chooseapath = int(input("Each cave leads to different places, pick one! 1 / 2 / 3: "))
+    if chooseapath == 1:
+        sea()
+    elif chooseapath == 2:
+        cave()
+    else:
+        ice()
 
-if chooseapath == 1:
-    sea()
-elif chooseapath == 2:
-    cave()
-else:
-    ice()
+firstmove()
 
 
